f7fec3f6ca25ad5f17219b4a28798816e03f8cd1
mrmoney10010-design <mr.money10010@gmail.com>
2026-02-13 18:56:18 -0500

    Update scanner.py

--- scanner.py
+++ scanner.py
@@ -8,5 +8,5 @@ results = subprocess.check_output(f"sudo nmap -sV {target}", shell=True).decode(
 with open("scan_report.txt", "w") as file:
     file.write(results)
 
-print("✅ scan complete! Results saved to scan_report.txt")  
+print("✅ scan complete! Results saved to scan_report.txt  ")  
 
